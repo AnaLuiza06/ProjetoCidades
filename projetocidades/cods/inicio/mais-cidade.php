dd28ba697200503a08dc6a573f21cd1f0e4cc7c0
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-09-12 08:56:14 -0300

    Criando Carrossel

---
+++ projetocidades/cods/inicio/mais-cidade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Projeto Cidades</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="../../css/style-inicio.css">
+</head>
+<body>
+
+	<header>
+		<div class="redes-sociais">
+			
+		</div>
+		<div>
+			<h2>Cidades</h2>
+			<ul>
+				<li><a href="#">Cidades</a></li>
+				<li><a href="#">Pontos Turísticos</a></li>
+				<li><a href="#">Comentários</a></li>
+			</ul>
+		</div>
+	</header>
+
+	<section id="inicio-cidades">
+		<img src="https://guiaviajarmelhor.com.br/wp-content/uploads/2021/03/cidades-sul-de-minas-gerais.png">
+	</section>
+
+	<section id="cards-links-informacoes">
+
+		<div class="card-informacoes" id="cardDados">
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+			  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+			</svg>
+			<h4>Dados</h4>
+		</div>
+
+		<div class="card-informacoes" id="cardHistoria">
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+			  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+			</svg>
+			<h4>História</h4>
+		</div>
+
+		<div class="card-informacoes" id="cardPontos">
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+			  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+			</svg>
+			<h4>Pontos</h4>
+		</div>
+
+		
+		<div class="card-informacoes" id="cardComentarios">
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+			  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+			</svg>
+			<h4>Comentários</h4>
+		</div>
+
+		<div class="card-informacoes" id="cardAddComentarios">
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+			  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+			</svg>
+			<h4>Meu Comentário</h4>
+		</div>
+	</section>
+
+	<section id="conteudo">
+		
+		<div id="conteudoHistoria">
+			<h2>História</h2>
+			<p>jwduhdhdu</p>
+			<p>idihweihfweu</p>
+		</div>
+
+		<div id="pontos">
+			<h2>História</h2>
+			<p>jwduhdhdu</p>
+			<p>idihweihfweu</p>
+		</div>
+
+		<div id="hidtoria">
+			<h2>História</h2>
+			<p>jwduhdhdu</p>
+			<p>idihweihfweu</p>
+		</div>
+
+		<div id="comentarios">
+			<h2>História</h2>
+			<p>jwduhdhdu</p>
+			<p>idihweihfweu</p>
+		</div>
+
+		<div id="add-comentario">
+			<h2>História</h2>
+			<p>jwduhdhdu</p>
+			<p>idihweihfweu</p>
+		</div>
+
+	</section>
+
+
+	<script>
+
+		const cardHistoria = document.querySelectorAll("#cardHistoria")
+		const cardPontos = document.querySelectorAll("#cardPontos")
+		const cardDados = document.querySelectorAll("#cardDados")
+		const cardComentarios = document.querySelectorAll("#cardComentarios")
+		const cardAddComentarios = document.querySelectorAll("#cardAddComentarios")
+
+		const conteudoHistoria = document.querySelector("#conteudoHistoria")
+		const conteudoPontos = document.querySelector("#conteudoPontos")
+		const conteudoDados = document.querySelector("#conteudoDados")
+		const conteudoComentarios = document.querySelector("#conteudoComentarios")
+		const conteudoAddComentarios = document.querySelector("#conteudoAddComentarios")
+
+		cardHistoria.addEventListener("click", () => {
+			cardHistoria.classList.add("active")
+			cardPontos.classList.remove("active")
+			cardDados.classList.remove("active")
+			cardComentarios.classList.remove("active")
+			cardAddComentarios.classList.remove("active")
+		})
+
+		cardPontos.addEventListener("click", () => {
+			cardHistoria.classList.remove("active")
+			cardPontos.classList.add("active")
+			cardDados.classList.remove("active")
+			cardComentarios.classList.remove("active")
+			cardAddComentarios.classList.remove("active")
+		})
+
+		cardDados.addEventListener("click", () => {
+			cardHistoria.classList.remove("active")
+			cardPontos.classList.remove("active")
+			cardDados.classList.add("active")
+			cardComentarios.classList.remove("active")
+			cardAddComentarios.classList.remove("active")
+		})
+
+		cardComentarios.addEventListener("click", () => {
+			cardHistoria.classList.remove("active")
+			cardPontos.classList.remove("active")
+			cardDados.classList.remove("active")
+			cardComentarios.classList.add("active")
+			cardAddComentarios.classList.remove("active")
+		})
+
+		conteudoAddComentarios.addEventListener("click", () => {
+			cardHistoria.classList.remove("active")
+			cardPontos.classList.remove("active")
+			cardDados.classList.remove("active")
+			cardComentarios.classList.remove("active")
+			cardAddComentarios.classList.add("active")
+		})
+            // function marcaCheckBox(card) {
+
+		
+	</script>
+
+</body>
+</html>
