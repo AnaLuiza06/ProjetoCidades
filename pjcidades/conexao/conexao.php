a1ee820d14a627d7dd511e5d620786b0ab31d7a9
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-12-05 08:34:37 -0300

    Subindo infinit

--- pjcidades/conexao/conexao.php
+++ pjcidades/conexao/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	$servidor = 'localhost';
-	$usuario = 'root';
-	$senha = '';
-	$banco = 'pj_cidades';
+	$servidor = 'sql213.epizy.com';
+	$usuario = 'epiz_32583505';
+	$senha = 'bCErLYf1aLjibm';
+	$banco = 'epiz_32583505_bd_cidade';
 
 	$cn = mysqli_connect($servidor, $usuario, $senha, $banco);
 
