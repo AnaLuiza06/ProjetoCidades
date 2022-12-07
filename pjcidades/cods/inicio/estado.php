c4c5ce84bf819bae76fd35e5bd05933bf8f14c3d
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-12-07 14:24:38 -0300

    Add files via upload

--- pjcidades/cods/inicio/estado.php
+++ pjcidades/cods/inicio/estado.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="../../css/style-inicio.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style-menu.css">
 </head>
 <body>
 	<!-- Incluir Menu -->
