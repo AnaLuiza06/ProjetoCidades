04cfad2b6f75051224345ff17794d6293c27c005
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-12-08 08:43:00 -0300

    arefazendo carrossel

--- pjcidades/js/carrossel.js
+++ pjcidades/js/carrossel.js
@@ -1,35 +1,36 @@
-		function mostrarAnterior(){
-			const elant = document.querySelectorAll(".card-regioes")
+function mostrarAnterior(){
+	const elant = document.querySelectorAll(".card-carrosel")
 
-			const cardsActive = document.querySelectorAll(".active")
-			const cardp1 = cardsActive[0].getAttribute("data-id");
+	const cardsActive = document.querySelectorAll(".active")
+	const cardp1 = Number(cardsActive[0].dataset.id)
 
-			if (cardp1 > 1) {
+	if (cardp1 > 0) {
 
-			// remover card
-				cardsActive[3].classList.remove('active')
+		// remover card
+		console.log(cardsActive)
+		cardsActive[2].classList.remove('active')
 
-			// adicionar card
-				const novocar = parseInt(cardp1) - 2
-				elant[novocar].classList.add('active')
-			}
-		}
+		// adicionar card
+		const novoCard = cardp1 - 1
+		elant[novoCard].classList.add('active')
+	}
+}
 
-		function mostrarProximo(){
+function mostrarProximo(){
 
-			const el = document.querySelectorAll(".container-carrossel")
-			const quantidade = parseInt(el.getAttribute("data-id"))
+	const el = document.querySelectorAll(".card-carrosel")
+	const quantidade = Number(el.length)
 
-			const cardsActives = document.querySelectorAll(".active")
-			const cardp4 = cardsActives[3].getAttribute("data-id")
+	const cardsActives = document.querySelectorAll(".active")
+	const cardp4 = Number(cardsActives[2].dataset.id)
 
-			if (cardp4 < quantidade) {
+	if (cardp4 + 1 < quantidade) {
+		// removendo card ativo
+        cardp1 = cardsActives[0];
+        cardp1.classList.remove('active');
 
-			// remover card
-				cardsActives[0].classList.remove('active')
-
-			// adicionar card
-				const novocard = parseInt(cardp4)
-				el[novocard].classList.add('active')
-			}
-		}
+		// adicionar card
+		const novocard = Number(cardp4 + 1)
+		el[novocard].classList.add('active')
+	}
+}
