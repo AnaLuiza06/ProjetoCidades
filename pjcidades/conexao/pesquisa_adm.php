ab40c8c9463492641af99f2159f4a028a5219462
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-12-08 21:47:29 -0300

    Add files via upload

--- pjcidades/conexao/pesquisa_adm.php
+++ pjcidades/conexao/pesquisa_adm.php
@@ -5,12 +5,11 @@
     $pagina = $_POST['pagina'];
     $pesquisa = $_POST['perquisar'];
 
-    $consulta = mysqli_query($cn, "SELECT * FROM $pagina WHERE nome like '$pesquisa'");
+    $consulta = mysqli_query($cn, "SELECT * FROM $pagina WHERE `nome` = '$pesquisa'");
 	$exibe = mysqli_fetch_all($consulta, MYSQLI_ASSOC);
 
     if(count($exibe) > 0){
-        print_r($pagina);
-        print_r($pesquisa);
+        print_r($exibe);
         //header("Location: ../cods/adm1/pages-menu/".$pagina.".php?cd=".$pesquisa."");
     }
     else{
